282b9f70e395ce1edc14c823cd65b810e8931ba3
hdg9400@gmail.com <hdg9400@gmail.com>
2017-08-09 15:23:51 +0900

    nav바 수정

--- food/forms.py
+++ food/forms.py
@@ -1,1 +1,10 @@
 #food/forms.py
+from django import forms
+
+from .models import Food
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Food
+        fields = ('', 'text',)
--- food/urls.py
+++ food/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
+    url(r'^/about$', views.about, name='about'),
+    url(r'^/service$', views.service, name='service'),
+    url(r'^/contact$', views.contact, name='contact'),
+    # url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+
 ]
+   
+   #url(r'^(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+   # url(r'^new/$', views.post_new, name='post_new'),
