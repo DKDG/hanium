8a64f91973c90cf567ad4163eabc86ae2ccec524
hdg9400@gmail.com <hdg9400@gmail.com>
2017-08-14 13:31:33 +0900

    ui 수정 및 객체 추가

--- hanium/settings.py
+++ hanium/settings.py
@@ -115,6 +115,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+#Email
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_PORT = 587
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
